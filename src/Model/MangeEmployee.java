da1260dae73ed6823d30722b4c1f368586440cfd
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2023-07-01 09:16:36 +0545

    Add files via upload

---
+++ src/Model/MangeEmployee.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Legion
+ */
+public class MangeEmployee {
+
+    public static String getEmployeeName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public static String getDepartment() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public static String getSalary() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    private String EmployeeName;
+    private String Department;
+    private int Salary;
+
+    public String Salary() {
+        int Salary;
+        return null;
+    }
+
+    public void Salary(int Salary) {
+        this.Salary = Salary;
+    }
+
+    public String EmployeeName() {
+        return EmployeeName;
+    }
+
+    public void Department(String Department) {
+        this.Department = Department;
+    }
+
+    public void setEmployeeName(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setDepartment(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setSalary(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+
+  
+}
